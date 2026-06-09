c74be0943a53543491e2bf8a63224391c5d2f7ac
Dmitriy Pushkin <Dmitriy.Pushkin@developertools.com>
2026-06-09 18:23:24 +0300

    Add yml

--- tests/test_button.py
+++ tests/test_button.py
@@ -1,10 +1,13 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 
 @pytest.fixture()
 def browser():
-    chrome_browser = webdriver.Chrome()
+    options = Options()
+    options.add_argument('--headless')
+    chrome_browser = webdriver.Chrome(options)
     return chrome_browser
 
 def test_button_exists(browser):
